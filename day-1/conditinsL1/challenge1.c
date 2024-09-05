68e8de04bf00b64682657b7f77820571be757654
Zakariae moufid <moufidzakariae3@gmai.com>
2024-09-05 14:16:01 +0200

    Paire ou Impaire

---
+++ day-1/conditinsL1/challenge1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+int main() {
+
+int N ;
+
+printf("veuillez saisir un nomber entier : ");
+scanf("%d",&N);
+
+if(N%2==0)
+    printf("%d est pair.");
+else
+    printf("%d est impair");
+
+
+return 0;
+}
