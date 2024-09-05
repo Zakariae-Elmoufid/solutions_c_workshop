3f15711a08ed99ad4beb19c93035371569c429ff
Zakariae moufid <moufidzakariae3@gmai.com>
2024-09-05 14:27:01 +0200

    Somme de Deux Valeurs

---
+++ day-1/conditinsL1/challenge3.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+int main(){
+
+char voyelle ;
+
+
+
+printf(" veuillez saisir n'importe lequel alphabet qui veux-tu : ");
+scanf("%c",&voyelle);
+
+switch(voyelle){
+  case 'A' :
+      printf(" %c est une voyelle",voyelle);
+  break;
+  case 'O':
+      printf(" %c est une voyelle",voyelle);
+  break;
+    case 'U':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'E':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'Y':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'I':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'a' :
+      printf(" %c est une voyelle",voyelle);
+  break;
+  case 'o':
+      printf(" %c est une voyelle",voyelle);
+  break;
+    case 'u':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'e':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'y':
+      printf("%c est une voyelle",voyelle);
+  break;
+    case 'i':
+      printf("%c est une voyelle",voyelle);
+  break;
+
+    default :
+        printf("%c n'est pas une voyelle",voyelle);
+}
+
+
+return 0;
+}
