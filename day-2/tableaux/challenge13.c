44549f1262d2db7979b5e9f3fc96d52590d888b7
zakariae moufid <moufidzakariae3@gmail.com>
2024-09-08 12:55:50 +0100

    Afficher les Éléments Impaire

---
+++ day-2/tableaux/challenge13.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+int N , T[10] ;
+int  i ;
+
+ // Demande à l'utilisateur de saisir le nombre d'éléments du tableau
+printf("saisir le nombre d'elements d'un tableau : ");
+scanf("%d",&N);
+
+// Saisie des éléments du tableau
+printf("saisir ces elements. \n");
+for(i=0;i<N;i++){
+    printf("T[%d] = ",i+1);
+    scanf("%d",&T[i]);
+}
+
+// afficher uniquement les element impair
+printf("les element du tableau impair sont :");
+for(i=0;i<N;i++){
+    if (T[i]%2!=0)
+        printf("%d ",T[i]);
+}
+return 0;
+}
