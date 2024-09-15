9b08efc41351d092bd6339a217c3319cdd531e28
zakariae moufid <moufidzakariae3@gmail.com>
2024-09-15 19:04:22 +0100

    Affichage d'une chaine

---
+++ day-3/string/challenge1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+   char caracter[50];
+
+   printf("Entrez une chaine de caracteres .\n");
+   scanf("%s",caracter);
+   
+   printf("-- %s .",caracter);
+
+
+    return 0;
+}
