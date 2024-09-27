882f9ab9079df618aaa1b035d4a2487c3d144333
zakariae moufid <moufidzakariae3@gmail.com>
2024-09-27 18:12:19 +0100

    Fonction de Fibonacci

--- day-2/Fonctions/challenge6.c
+++ day-2/Fonctions/challenge6.c
@@ -2,17 +2,34 @@
 
 int fibonacci(int n ){
 
-    if(n==0)
-     return 0 ;
-      else if (n==1)
-     return 1 ;
-     else
-     return (fibonacci(n-1) + fibonacci(n-2));
+ int i , U0 , U1 ,U;
+ 
+    U0 = 0 ;
+    U1 = 1 ;
+
+if(n==0){
+    return U0;
+}
+ if(n==1){
+    return U1;
+}
+
+   for(i=2;i<=n;i++){
+    U  = U0+U1;
+    U0 = U1 ;
+    U1 = U ;
+ }
+
+
+ return U ;
+
 }
 int main(){
-    int n ;
-    n = 9 ;
-    printf("%d",fibonacci(n));
+    int n , i;
+    n = 5 ;
+
+    for(i=0;i<=n;i++)
+    printf("%d ",fibonacci(i));
     return 0;
 }
 
